eca8c29025285c9d2acfe7a54bf2961bfc0ddf7f
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-10-13 13:43:12 -0500

    Finished task 2

--- index.js
+++ index.js
@@ -86,10 +86,20 @@ Person.prototype.toString = function(){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function(gallons){
+  return this.tank = this.tank + gallons;
+}
+
+// const myCorolla = new Car('Corolla', 20);
+// console.log(myCorolla);
+// console.log(myCorolla.fill(20));
 
 /*
   TASK 3
